c67ce13e211d4de926b6bc29a1e1aa4241696415
Arthur Rocha <arthur_stads@hotmail.com>
2019-06-05 18:30:48 -0300

    Initial project

---
+++ desafio-consulta-remedios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  desafio-consulta-remedios
+//
+//  Created by Arthur Rocha on 03/06/19.
+//  Copyright © 2019 Arthur Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ desafio-consulta-remediosTests/desafio_consulta_remediosTests.swift
@@ -0,0 +1,34 @@
+//
+//  desafio_consulta_remediosTests.swift
+//  desafio-consulta-remediosTests
+//
+//  Created by Arthur Rocha on 03/06/19.
+//  Copyright © 2019 Arthur Rocha. All rights reserved.
+//
+
+import XCTest
+@testable import desafio_consulta_remedios
+
+class desafio_consulta_remediosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
