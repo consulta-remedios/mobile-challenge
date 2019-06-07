0b9ff3fcaa0c76ed0245b6dfcc84d174d7e63235
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-06-06 21:13:48 -0300

    Add the Item and Cart models
    The Item is a Codable struct representing an item in the API,
    the Cart handles the shopping feature of the app.

---
+++ MobileChallenge/MobileChallenge/Model/Cart.swift
@@ -0,0 +1,16 @@
+//
+//  Cart.swift
+//  MobileChallenge
+//
+//  Created by Tiago Maia Lopes on 05/06/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
+
+/// The user's cart.
+struct Cart {
+
+    /// The items currently in the cart.
+    private(set) var items: [Item] = []
+}
---
+++ MobileChallenge/MobileChallenge/Model/Item.swift
@@ -0,0 +1,40 @@
+//
+//  Item.swift
+//  MobileChallenge
+//
+//  Created by Tiago Maia Lopes on 05/06/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
+
+/// An item from the store.
+struct Item: Codable {
+
+    // MARK: Parameters
+
+    /// The identifier of this item in the store.
+    let identifier: Int
+
+    /// The name of the item.
+    let name: String
+
+    /// The price of the item.
+    let price: Double
+
+    /// The platform of the item.
+    let platform: String
+
+    /// The path of the cover image in the server.
+    let imagePath: String
+
+    // MARK: Coding keys
+
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case name
+        case price
+        case platform
+        case imagePath = "image"
+    }
+}
