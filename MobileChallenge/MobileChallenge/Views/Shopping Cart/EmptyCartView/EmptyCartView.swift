4a44cafb48857364583cda4c44ae399bfbbb5f44
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-06-08 20:29:22 -0300

    Display a view informing the user that the shopping cart is empty

---
+++ MobileChallenge/MobileChallenge/Views/Shopping Cart/EmptyCartView/EmptyCartView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCartView.swift
+//  MobileChallenge
+//
+//  Created by Tiago Maia Lopes on 08/06/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+/// Informs the user that the shopping cart is empty.
+class EmptyCartView: UIView {
+
+    // MARK: Properties
+
+    /// The button used to go to the listing controller.
+    @IBOutlet weak var keepBuyingButton: UIButton!
+}
