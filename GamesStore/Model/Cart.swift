091178d10bef651371c74735666cf0c356646be0
Márcio Brufatto <mhbrufatto@gmail.com>
2019-06-12 00:41:12 -0300

    Criada a estrutura básica da aplicação da loja de jogos

---
+++ GamesStore/Model/Cart.swift
@@ -0,0 +1,73 @@
+//
+//  Cart.swift
+//  GamesStore
+//
+//  Created by Marcio Habigzang Brufatto on 12/06/19.
+//  Copyright © 2019 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Cart: Object {
+    
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String = ""
+    @objc dynamic var description: String = ""
+    @objc dynamic var price: Double = 0.0
+    @objc dynamic var score: Int = 0
+    @objc dynamic var platform: String = ""
+    @objc dynamic var image: String = ""
+    @objc dynamic var count: Int = 0
+    
+    func addGameInCart(game: Game) {
+        let realm = try! Realm()
+        let cart = realm.objects(Cart.self).filter("id = %d", game.id).first
+        
+        if cart == nil {
+            try! realm.write {
+                let cart = Cart()
+                cart.id = game.id
+                cart.name = game.name
+                cart.description = game.description
+                cart.price = game.price
+                cart.score = game.score
+                cart.platform = game.platform
+                cart.image = game.image
+                cart.count += 1
+                realm.add(cart)
+            }
+            return
+        }
+        
+        try! realm.write {
+            cart!.count += 1
+            realm.add(cart!)
+        }
+    }
+    
+    func calculateShipping(total:Double) -> Double {
+        if total > 250 {
+            return 0.0
+        }
+        
+        let realm = try! Realm()
+        let gamesInCart = realm.objects(cart.self)
+        var shippingValue: Double = 0.0
+        for game in gamesInCart {
+            shippingValue += Double(game.count) * 10.0
+        }
+        return shippingValue
+    }
+    
+    func calculateTotal() -> Double {
+        let realm = try! Realm()
+        let gameInCart = realm.objects(Cart.self)
+        var total: Double = 0.0
+        for game in gameInCart {
+            total += Double(game.count) * game.price
+        }
+        
+        return total
+    }
+}
---
+++ GamesStore/Service/Service.swift
@@ -0,0 +1,60 @@
+//
+//  Service.swift
+//  GamesStore
+//
+//  Created by Marcio Habigzang Brufatto on 12/06/19.
+//  Copyright © 2019 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+class Service {
+    static let shared = Service()
+    
+    let baseUrl = "https://game-checkout.herokuapp.com"
+    let token = "QceNFo1gHd09MJDzyswNqzStlxYGBzUG"
+    
+    func fetchGames(page: Int = 0, completionHandler: @escaping([Game?], NSError?) ->()) {
+        
+        let headers: HTTPHeaders = [
+            "TOKEN": token
+        ]
+        
+        Alamofire.request(baseUrl + "/games", method: .get, headers: headers)
+            .responseArray { (response: DataResponse<[Game]>) in
+                switch response.result {
+                case .success:
+                    if response.response?.statusCode == 202 {
+                        completionHandler([], nil)
+                    } else if response.response?.statusCode == 200 {
+                        completionHandler(response.result.value!, nil)
+                    }
+                case .failure(let error):
+                    completionHandler([], error as NSError)
+                }
+        }
+    }
+    
+    func checkout(page: Int = 0, completionHandler: @escaping(Bool, NSError?) ->() ){
+        let headers: HTTPHeaders = [
+            "TOKEN": token
+        ]
+        
+        Alamofire.request(baseUrl + "/checkout" , method: .post, headers: headers)
+            .responseString { response in
+                switch response.result {
+                case .success:
+                    if response.response?.statusCode == 202 {
+                        completionHandler(false, nil)
+                    } else if response.response?.statusCode == 200 {
+                        completionHandler(true, nil )
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    completionHandler(false, error as NSError)
+                }
+        }
+    }
+}
